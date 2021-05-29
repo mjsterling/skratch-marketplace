3aa9a435ffbf995062879a30314dcfd6b3e62996
mjsterling <mjsterling93@gmail.com>
2021-05-30 00:01:33 +1000

    CRUD is operational.

--- app/helpers/trades_helper.rb
+++ app/helpers/trades_helper.rb
@@ -1,2 +1,14 @@
 module TradesHelper
+    def find_service(id, field)
+        service = Service.find_by(service_id: id)
+        data = ''
+        case field
+        when 'name'
+            data = service.name
+        when
+            data = 
+        end
+
+        data
+    end
 end
